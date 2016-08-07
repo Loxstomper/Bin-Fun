15cd09b1d41a09edc83536be11d7d0ba65fda570
Loxstomper <lox.ash@hotmail.com>
2016-08-07 20:09:38 +1000

    more pseduo

--- AdafruitMatrix/Snake/snake.ino
+++ AdafruitMatrix/Snake/snake.ino
@@ -6,3 +6,28 @@
 // [][]
 // []  []
 //   [][]
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+int head_x = 0;
+int head_y = 0;
+
+int body [3] [3] [3];
+body[0].
+
+
+
+
+// OR
+// create a data type
+struct POS
+{
+    int x;
+    int y;
+}
+
+POS body [10];
+body[1].x = 5;
+body[1].y = 3;
+
+// have the head as element 0 when length gets increased add one to the array and figure out the coords
